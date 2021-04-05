1aa473140dcb1c0b59f5f8d8258521bfd3047a5f
lb-beep <77352348+lb-beep@users.noreply.github.com>
2021-04-05 12:49:37 -0600

    Add files via upload

---
+++ Controllers/ImageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace ShoppingCart.Controllers
+{
+    public class ImageController : Controller
+    {
+        public IActionResult GetAllImages(string imageName)
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace ShoppingCart.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult GetAllProducts(string productName)
+        {
+            return View();
+        }
+    }
+ 
+  
+    
+}
